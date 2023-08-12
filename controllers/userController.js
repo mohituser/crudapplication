5fd2ebc808d175ef9c3fa57a93a4709003f11c9e
mohituser <122739741+mohituser@users.noreply.github.com>
2023-08-12 10:45:31 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -178,7 +178,7 @@ exports.signup=async(req,res,next)=>{
         
             }
 
- cron.schedule("20 10 * * *",()=>{
+ cron.schedule("55 10 * * *",()=>{
                 // console.log("hi i am running...........")
                 this.fetchUser();
      })
